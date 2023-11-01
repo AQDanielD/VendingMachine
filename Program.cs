474b5afcbfe43bf6d4571d970def9fbfe94f0949
Daniel Davies <AQ232596@aquinas.ac.uk>
2023-11-01 19:29:02 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine
+{
+    internal class Program
+    {
+        struct Item
+        {
+            public int Id;
+            public string Name;
+            public int Stock;
+            public double Price;
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<object> list = new List<object>();
+
+
+        }
+    }
+}
